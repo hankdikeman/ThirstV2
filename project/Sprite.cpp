97396069db2fbc17cd05bf47e5bc0ba9ceb460a3
hankdikeman <dikem003@umn.edu>
2022-01-03 15:03:48 -0600

    additional tests for Sprite class

--- project/Sprite.cpp
+++ project/Sprite.cpp
@@ -5,6 +5,7 @@
 
 // stdlib headers
 #include <array>
+#include <random>
 #include <vector>
 #include <stdbool.h>
 #include <iostream>
@@ -26,11 +27,12 @@
 #define WINDOW_HEIGHT (640)
 
 // sprite definitions
-#define SPRITE_SIZE (32)
-#define NUM_SPRITES (20)
+#define SPRITE_SIZE (64)
+#define NUM_SPRITES (5)
 
 // duration definitions
-#define NUM_FRAMES (10)
+#define NUM_FRAMES (1000)
+#define NUM_CYCLES (2)
 
 // pointers to windows, textures, music
 SDL_Window* window;
@@ -38,6 +40,10 @@ SDL_Renderer* renderer;
 SDL_Texture* texture;
 
 int main(int argc, const char *argv[]) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(-5, 5);
+
     // initialize video and timer, catch errors
     SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER);
     IMG_Init(IMG_INIT_JPG);
@@ -51,7 +57,7 @@ int main(int argc, const char *argv[]) {
     // temporary var to load image
     SDL_Surface *temp;
     // load image to temp var
-    temp = IMG_Load("Resources/CH_Sprite.jpeg");
+    temp = IMG_Load("Resources/PCSprite.png");
     // format surfaces
     texture = SDL_CreateTextureFromSurface(renderer, temp);
     // free temp
@@ -63,17 +69,32 @@ int main(int argc, const char *argv[]) {
 
     // list of sprites
     std::vector<Sprite> spritelist;
-    std::array<int,2> size = {32, 32};
+    std::array<int,2> drawsize = {SPRITE_SIZE, SPRITE_SIZE};
     std::array<int,2> position = {0, 0};
     
     // initialize arrays
     for (int idx = 0; idx < NUM_SPRITES; idx++) {
         // add sprite to vector
-        Sprite newSprite = Sprite(position, size, texture);
+        Sprite newSprite = Sprite(position, drawsize, NUM_CYCLES, texture);
         spritelist.push_back(newSprite);
         // change position
-        position[0] += 32;
-        position[1] += 32;
+        position[0] += SPRITE_SIZE;
+        position[1] += SPRITE_SIZE;
+    }
+
+    // wait for key press before rendering further
+    SDL_Event event;
+    bool cont_req = false;
+    while (!cont_req){
+        // cycle through event queue and process
+        while (SDL_PollEvent(&event)){
+            switch(event.type) {
+                // trigger if a key was pressed
+                case SDL_KEYDOWN:
+                    cont_req = true;
+                    break;
+            }
+        }
     }
 
     // game loop
@@ -83,26 +104,23 @@ int main(int argc, const char *argv[]) {
         // render loop
         for (int idx = 0; idx < NUM_SPRITES; idx++) {
             SDL_RenderCopy(renderer, spritelist[idx].get_texture(), spritelist[idx].get_srcrect(), spritelist[idx].get_dstrect());
+            if (ctr % 10 == 0) {
+                // increment sprite frame
+                spritelist[idx].increment_frame();
+                // random width
+                spritelist[idx].set_width(spritelist[idx].get_width() + distrib(gen));
+                // random height
+                spritelist[idx].set_height(spritelist[idx].get_height() + distrib(gen));
+                // random x
+                spritelist[idx].set_xpos(spritelist[idx].get_xpos() + distrib(gen));
+                // random y
+                spritelist[idx].set_ypos(spritelist[idx].get_ypos() + distrib(gen));
+            }
         }
 
         // render all to window
         SDL_RenderPresent(renderer);
-        SDL_Delay(2);
-
-        // wait for key press before rendering further
-        SDL_Event event;
-        bool cont_req = false;
-        while (!cont_req){
-            // cycle through event queue and process
-            while (SDL_PollEvent(&event)){
-                switch(event.type) {
-                    // trigger if a key was pressed
-                    case SDL_KEYDOWN:
-                        cont_req = true;
-                        break;
-                }
-            }
-        }
+        SDL_Delay(20);
     }
 
     // destroy window and delete all SDL vars
