e3c6556ad5acfe7b9af93594babe1923f4798c91
hankdikeman <dikem003@umn.edu>
2022-01-03 12:43:12 -0600

    restore proper names of entity library

---
+++ project/Static.cpp
@@ -0,0 +1,11 @@
+/*
+ * descr: compilation test file for abstract class `Static`
+ * author: Henry Dikeman
+ */
+
+#include <SDL.h>
+#include "Static.h"
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
---
+++ project/Static.h
@@ -0,0 +1,46 @@
+/*
+ * descr: header file for Static entity class. Animations which do not have cycles
+ * author: Henry Dikeman
+ */
+
+// stdlib headers
+#include <array>
+
+// externlib headers
+#include <SDL.h>
+
+// internlib headers
+#include "Drawable.h"
+
+#ifndef STATIC_H
+#define STATIC_H
+
+class Static: public Drawable {
+    public:
+        // constructors
+        Static(std::array<int,2> pos, std::array<int,2> size, SDL_Texture* text);
+        // destructors
+        virtual ~Static();
+        // implemented virtual method
+        virtual const SDL_Rect* get_srcrect();
+};
+
+// int constructor
+Static::Static(std::array<int,2> pos, std::array<int,2> size, SDL_Texture* text) : 
+    Drawable(pos, size, text) {}
+
+// destructor
+Static::~Static() {}
+
+// provide src rectangle for renderer
+const SDL_Rect* Static::get_srcrect() {
+    // update source rectangle from position data
+    srcrect.x = 0;
+    srcrect.y = 0;
+    srcrect.w = drawsize[0];
+    srcrect.h = drawsize[1];
+    // return rectangle
+    return &srcrect;
+}
+
+#endif
