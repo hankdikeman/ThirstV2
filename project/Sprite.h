d858e242cf382dfbaf01924f14f98c1eb024b4a0
hankdikeman <dikem003@umn.edu>
2022-01-03 15:03:16 -0600

    alternative constructor with numframe arg

--- project/Sprite.h
+++ project/Sprite.h
@@ -17,30 +17,47 @@
 
 class Sprite: public Drawable {
     protected:
+        std::array<int, 2> srcsize;
         int num_cycles;
-        int curr_cycle;
+        int curr_frame;
     public:
         // constructors
-        Sprite(std::array<int, 2> pos, std::array<int, 2> size, SDL_Texture* text);
+        Sprite(std::array<int, 2> pos, std::array<int, 2> dsize, std::array<int, 2> ssize, SDL_Texture* text);
+        Sprite(std::array<int, 2> pos, std::array<int, 2> dsize, int n_cyc, SDL_Texture* text);
         // destructors
         virtual ~Sprite();
         // implemented virtual method
         virtual const SDL_Rect* get_srcrect();
+        // animation cycle iteration
+        void increment_frame();
 };
 
-// int constructor
-Sprite::Sprite(std::array<int, 2> pos, std::array<int, 2> size, SDL_Texture* text) : 
-    Drawable(pos, size, text)
-    {
+// source size constructor
+Sprite::Sprite(std::array<int, 2> pos, std::array<int, 2> dsize, std::array<int, 2> ssize, SDL_Texture* text) :
+    Drawable(pos, dsize, text),
+    srcsize(ssize) {
         // get dimensions of spritesheet
         int spritesheet_w, spritesheet_h;
         SDL_QueryTexture(texture, NULL, NULL, &spritesheet_w, &spritesheet_h);
         // set number of cycles with spritesheet width and width
-        num_cycles = spritesheet_w / drawsize[0];
-
+        num_cycles = spritesheet_w / srcsize[0];
         // set current cycle to 0
-        curr_cycle = 0;
-    }
+        curr_frame = 0;
+}
+
+// cycle specified constructor
+Sprite::Sprite(std::array<int, 2> pos, std::array<int, 2> dsize, int n_cyc, SDL_Texture* text) :
+    Drawable(pos, dsize, text),
+    num_cycles(n_cyc) {
+        // get dimensions of spritesheet
+        int spritesheet_w, spritesheet_h;
+        SDL_QueryTexture(texture, NULL, NULL, &spritesheet_w, &spritesheet_h);
+        // set number of cycles with spritesheet width and width
+        srcsize[0] = spritesheet_w / num_cycles;
+        srcsize[1] = spritesheet_h;
+        // set current cycle to 0
+        curr_frame = 0;
+}
 
 // destructor
 Sprite::~Sprite() {}
@@ -48,14 +65,16 @@ Sprite::~Sprite() {}
 // provide src rectangle for renderer
 const SDL_Rect* Sprite::get_srcrect() {
     // update source rectangle from position data
-    srcrect.x = curr_cycle*drawsize[0];
+    srcrect.x = curr_frame*srcsize[0];
     srcrect.y = 0;
-    srcrect.w = drawsize[0];
-    srcrect.h = drawsize[1];
-    // update cycle number
-    curr_cycle = (curr_cycle+1) % num_cycles;
+    srcrect.w = srcsize[0];
+    srcrect.h = srcsize[1];
     // return rectangle
     return &srcrect;
+}
+
+void Sprite::increment_frame() {
+    curr_frame = (curr_frame + 1) % num_cycles;
 }
 
 #endif
