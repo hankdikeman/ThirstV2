f2d69342affcc1c938c822988d49ce05733a24df
hankdikeman <dikem003@umn.edu>
2022-01-08 17:10:27 -0600

    EntityList container class

--- project/EntityList.h
+++ project/EntityList.h
@@ -5,8 +5,11 @@
 
 // stdlib headers
 #include <iostream>
+#include <memory>
 #include <array>
 #include <vector>
+#include <algorithm>
+#include <stdbool.h>
 
 // externlib headers
 #include <SDL.h>
@@ -20,8 +23,47 @@
 
 class EntityList {
     private:
-        vector<Sprite> e_list;
+        std::vector< std::shared_ptr<Sprite> > enemy_list;
+        std::shared_ptr<Sprite> player;
+    public:
+        // constructors
+        EntityList();
+        EntityList(std::shared_ptr<Sprite>& player);
+        EntityList(std::shared_ptr<Sprite>& player, std::vector<std::shared_ptr<Sprite> >& enemy_list);
 
+        // destructor
+        ~EntityList() {}
+
+        // getters
+        std::vector<std::shared_ptr<Sprite> >& get_enemy_list() { return enemy_list; }
+        std::shared_ptr<Sprite>& get_player() { return player; }
+
+        // setters
+        void add_enemy(std::shared_ptr<Sprite>& enemy) { enemy_list.push_back(enemy); }
+
+        // delete methods
+        bool delete_enemy(std::shared_ptr<Sprite>& enemy);
 };
+
+// empty constructor
+EntityList::EntityList() {}
+
+// player-only constructor
+EntityList::EntityList(std::shared_ptr<Sprite>& player) :
+    player(player) {}
+
+// enemy list and player constructor
+EntityList::EntityList(std::shared_ptr<Sprite>& player, std::vector<std::shared_ptr<Sprite> >& enemy_list) :
+    enemy_list(enemy_list),
+    player(player) {}
+
+bool EntityList::delete_enemy(std::shared_ptr<Sprite>& enemy) {
+    // store initial size
+    int original_size = enemy_list.size();
+    // erase remove to remove enemies
+    enemy_list.erase(std::remove(enemy_list.begin(), enemy_list.end(), enemy), enemy_list.end());
+    // return success flag
+    return original_size == enemy_list.size()-1;
+}
 
 #endif
