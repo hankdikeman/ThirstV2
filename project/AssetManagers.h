18ab83bfa8b028a676605b25b6537981eb069bcb
hankdikeman <dikem003@umn.edu>
2022-01-08 17:54:58 -0600

    asset manager and factory template files

---
+++ project/AssetManagers.h
@@ -0,0 +1,30 @@
+/*
+ * descr: texture and sound manager classes
+ * author: Henry Dikeman
+ */
+
+// stdlib headers
+#include <iostream>
+#include <bitset>
+#include <memory>
+#include <array>
+#include <stdbool.h>
+
+// externlib headers
+#include <SDL.h>
+
+// internlib headers
+#include "Sprite.h"
+
+#ifndef ASSETMANAGERS_H
+#define ASSETMANAGERS_H
+
+class TextureManager {
+
+};
+
+class SoundManager {
+
+};
+
+#endif
---
+++ project/EntityFactory.h
@@ -0,0 +1,28 @@
+/*
+ * descr: entity factory class
+ * author: Henry Dikeman
+ */
+
+// stdlib headers
+#include <iostream>
+#include <bitset>
+#include <memory>
+#include <array>
+#include <stdbool.h>
+
+// externlib headers
+#include <SDL.h>
+
+// internlib headers
+#include "Sprite.h"
+#include "AssetManagers.h"
+
+#ifndef ENTITYFACTORY_H
+#define ENTITYFACTORY_H
+
+class EntityFactory {
+
+
+};
+
+#endif
