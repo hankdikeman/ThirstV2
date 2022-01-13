5f4f9c42605dc39fd2b55c01243bc9cd4b8fff1b
hankdikeman <dikem003@umn.edu>
2022-01-12 20:37:17 -0600

    core Engine input handling functionality

--- project/GameEngine.h
+++ project/GameEngine.h
@@ -25,8 +25,7 @@
 
 enum class GameState { 
     QUIT = -1,
-    RUNNING = 0,
-    PAUSED = 1
+    RUNNING = 0
 };
 
 class Engine {
@@ -36,24 +35,60 @@ class Engine {
         std::unique_ptr<EntityFactory> entFact;
 
     public:
+        // initialize tilemap with data
         void populate_map();
+        // process input events
         GameState input();
+        // execute one game logic step
         void step();
+        // render to screen
         void render(SDL_Renderer* renderer);
 };
 
 void Engine::populate_map() {
+    // call seed generation function
+    // procedurally generate sand texture
+    // procedurally generate oases
+        // make enemies for each oasis
 
 }
 
+// process input provided by user
 GameState Engine::input() {
+    // declare event object and pop from queue
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch(event.type) {
+            // if quit, return quit code
+            case SDL_QUIT:
+                return GameState::QUIT;
+                break;
+            // if keydown, handle keydown event
+            case SDL_KEYDOWN:
+                this->handle_keypress(event);
+                break;
+            // mouse button functionality to be added later
+            case SDL_MOUSEBUTTONDOWN:
+                this->handle_mousepress(event);
+                break;
+        }
+    }
+    // default return is normal running
     return GameState::RUNNING;
 }
 
+// empty keypress handling function
+void Engine::handle_keypress(event) {}
+
+// empty mousepress handling function
+void Engine::handle_mousepress(event) {}
+
+// game logic step forward
 void Engine::step() {
 
 }
 
+// render data onto background
 void render(SDL_Renderer* renderer) {
 
 }
